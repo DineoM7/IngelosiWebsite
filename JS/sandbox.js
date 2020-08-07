8432a05f999288e1354af2161fa2c409d778d5fe
iammosa <ramosamosa@gmail.com>
2020-08-07 13:51:36 +0200

    moblie links contact form

--- JS/sandbox.js
+++ JS/sandbox.js
@@ -1,8 +1,8 @@
 function openNav() {
-    document.getElementById("myNav").style.display = "block";
-  }
+  document.getElementById("myNav").style.display = "block";
+}
   
-  function closeNav() {
-    document.getElementById("myNav").style.display = "none";
-  }
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
   
